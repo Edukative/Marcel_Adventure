f9f9fec0451efbf562c8c8313861284079035949
Edukative <56083723+Edukative@users.noreply.github.com>
2020-01-30 17:23:57 +0100

    reimported project

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+
+    Transform Ruby;
+    Rigidbody2D RubyRB;
+    public float RVel;
+    Camera Cam;
+    Player_Controller1 RUBY;
+    public float moving = 0;
+
+
+    void Start()
+    {
+
+        
+        Ruby = GameObject.Find("Ruby").GetComponent<Transform>();
+        RubyRB = GameObject.Find("Ruby").GetComponent<Rigidbody2D>();
+        RUBY = GameObject.Find("Ruby").GetComponent<Player_Controller1>();
+
+        Cam = GetComponent<Camera>();
+
+    }
+
+    void Update()
+    {
+
+        transform.position = Ruby.position;
+        transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+
+        RVel = Mathf.Pow(RUBY.Match20 * RUBY.horizontal * Time.deltaTime * RUBY.Match20 * RUBY.horizontal * Time.deltaTime 
+                         + RUBY.Match20 * RUBY.vertical * Time.deltaTime * RUBY.Match20 * RUBY.vertical * Time.deltaTime, 0.5f);
+
+        Debug.Log(RVel * 4);
+        
+        if(Mathf.Approximately(RUBY.Match20 * RUBY.horizontal, 0.0f) && Mathf.Approximately(RUBY.Match20 * RUBY.vertical, 0.0f))
+        {
+
+            moving += -0.02f;
+
+        }
+        else
+        {
+
+            moving += 0.02f;
+
+        }
+
+        moving = Mathf.Clamp(moving, 0.0f, 1.0f);
+
+        Cam.orthographicSize = Mathf.Lerp(5, 7, Mathf.Clamp01(RVel * 4) * moving);
+
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/Enemy_controller.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_controller : MonoBehaviour
+{
+
+    public float speed;
+    Rigidbody2D rb2d;
+    public bool isVertical;
+
+    float timer;
+    int direction = 1;
+    public float changeTimer = 3.0f;
+
+    Animator anim;
+    Vector2 looking = Vector2.right;
+
+
+    void Start()
+    {
+
+        rb2d = GetComponent<Rigidbody2D>();
+        timer = changeTimer;
+        anim = GetComponent<Animator>();
+
+
+    }
+
+    void Update()
+    {
+
+        speed = 5;
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+
+            looking.Set(move.x, move.y);
+            looking.Normalize();
+
+        }
+
+        anim.SetFloat("look X", looking.x);
+        anim.SetFloat("look Y", looking.y);
+        anim.SetFloat("Speed", move.magnitude);
+
+        Vector2 position = rb2d.position;
+        position += move * speed * Time.deltaTime;
+
+
+
+
+        timer += -Time.deltaTime;
+        if(timer < 0)
+        {
+
+            direction = -direction;
+            timer = changeTimer;
+
+        }
+
+        
+        if (isVertical)
+        {
+
+            position.y += Time.deltaTime * speed * direction;
+
+            anim.SetFloat("MoveX", 0);
+            anim.SetFloat("MoveY", direction);
+
+        }
+        else
+        {
+
+            position.x += Time.deltaTime * speed * direction;
+
+            anim.SetFloat("MoveX", direction);
+            anim.SetFloat("MoveY", 0);
+            
+        }
+
+        rb2d.MovePosition(position);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+
+        Player_Controller1 player = other.gameObject.GetComponent<Player_Controller1>();
+
+        if(player != null)
+        {
+
+            player.ChangeHealth(-1);
+
+        }
+
+
+    }
+
+}
